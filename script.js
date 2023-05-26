d94b4f671ed80e952e9955cda3a2ecb1dbd3a66d
Piotr Materna <piotr@pmat.pl>
2023-05-26 15:55:12 +0200

    added new functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello from Nerdbord!')
+function main() {
+  const boxNode = document.querySelector(".box");
+  const borderColors = ["tomato", "royalblue", "magenta", "pink"];
+
+  let i = 0;
+
+  function handleBorderColorChange() {
+    boxNode.style.borderColor = borderColors[i];
+    i = (i + 1) % borderColors.length;
+    if (i === 0) clearInterval(timer);
+  }
+
+  timer = setInterval(handleBorderColorChange, 3000);
+}
+
+main();
